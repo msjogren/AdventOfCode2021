7eb043efe70165a7bd688c98a37b1c74635440f0
Mattias Sjögren <msj@trackman.com>
2021-12-02 13:33:24 +0100

    Solution for day 2

---
+++ day02/Program.cs
@@ -0,0 +1,23 @@
+﻿var input = File.ReadAllLines("input.txt")
+                .Select(line => line.Split())
+                .Select(parts => (direction: parts[0], x: int.Parse(parts[1])));
+
+// Part 1
+var forwards = input.Where(cmd => cmd.direction == "forward").Sum(cmd => cmd.x);
+var ups      = input.Where(cmd => cmd.direction == "up").Sum(cmd => cmd.x);
+var downs    = input.Where(cmd => cmd.direction == "down").Sum(cmd => cmd.x);
+Console.WriteLine(forwards * (downs - ups));
+
+// Part 2
+var (aim, depth, xpos) = (0L, 0L, 0L);
+foreach (var cmd in input)
+{
+    (aim, depth, xpos) = cmd.direction switch
+    {
+        "forward" => (aim,         depth + aim * cmd.x, xpos + cmd.x),
+        "up"      => (aim - cmd.x, depth,               xpos), 
+        "down"    => (aim + cmd.x, depth,               xpos),
+        _         => throw new InvalidOperationException()
+    };    
+}
+Console.WriteLine(xpos * depth);
