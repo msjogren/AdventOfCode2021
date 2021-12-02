85f857a9b6dc9585edd1ecd3d33a438fc2e2e8b9
Mattias Sjögren <msj@trackman.com>
2021-12-02 13:24:19 +0100

    Solution for day 1

---
+++ day01/Program.cs
@@ -0,0 +1,14 @@
+﻿var depths = File.ReadAllLines("input.txt")
+                 .Select(line => int.Parse(line));
+
+// Part 1
+var incs = depths.Zip(depths.Skip(1))
+                 .Count(pair => pair.Second > pair.First);
+Console.WriteLine(incs);
+
+// Part 2
+var windows = depths.Zip(depths.Skip(1), depths.Skip(2))
+                    .Select(tr => tr.First + tr.Second + tr.Third);
+var incs2 = windows.Zip(windows.Skip(1))
+                   .Count(pair => pair.Second > pair.First);
+Console.WriteLine(incs2);
