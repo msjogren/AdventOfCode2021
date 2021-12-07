a10aa7ce708caf1b416d81265664de01141b65f6
Mattias Sjögren <msj@trackman.com>
2021-12-08 00:10:51 +0100

    Solution for day 7

---
+++ day07/Program.cs
@@ -0,0 +1,17 @@
+﻿var input = File.ReadAllText("input.txt")
+                .Split(',')
+                .Select(int.Parse);
+
+long Solve(Func<int, int, long> cost)
+    => Enumerable.Range(input.Min(), input.Max() - input.Min() + 1)
+                 .Min(target => input.Sum(crab => cost(crab, target)));
+
+// Part 1
+Console.WriteLine(Solve((crabPos, targetPos) => Math.Abs(crabPos - targetPos)));
+
+// Part 2
+// 1 + 2 + ... + (n-1) + n = n(n+1)/2
+Console.WriteLine(Solve((crabPos, targetPos) => {
+    long n = Math.Abs(crabPos - targetPos);
+    return n * (n + 1) / 2;
+}));
