8d3801a07ed2627471d00303e8b132eaa3e06a94
Mattias Sjögren <msj@trackman.com>
2021-12-07 09:49:24 +0100

    Solution for day 6

---
+++ day06/Program.cs
@@ -0,0 +1,24 @@
+﻿var input = File.ReadAllText("input.txt");
+var initialState = input.Split(',').Select(int.Parse);
+
+long Solve(int days)
+{
+    var fishCountByAge = new long[9];
+    foreach (var i in initialState)
+    {
+        fishCountByAge[i]++;
+    }
+
+    for (int day = 0; day < days; day++)
+    {
+        var newFishCount = fishCountByAge[0];
+        Array.Copy(fishCountByAge, 1, fishCountByAge, 0, 8);
+        fishCountByAge[6] += newFishCount;
+        fishCountByAge[8] = newFishCount;
+    }
+
+    return fishCountByAge.Sum();
+}
+
+Console.WriteLine(Solve(80));
+Console.WriteLine(Solve(256));
