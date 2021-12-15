b93fd300f6212cc503cb1d8b6369e219eda39769
Mattias Sjögren <msj@trackman.com>
2021-12-16 00:13:51 +0100

    Solution for day 10
    Simplified day 7

--- day07/Program.cs
+++ day07/Program.cs
@@ -2,16 +2,13 @@
                 .Split(',')
                 .Select(int.Parse);
 
-long Solve(Func<int, int, long> cost)
+int Solve(Func<int, int> cost)
     => Enumerable.Range(input.Min(), input.Max() - input.Min() + 1)
-                 .Min(target => input.Sum(crab => cost(crab, target)));
+                 .Min(target => input.Sum(crab => cost(Math.Abs(crab - target))));
 
 // Part 1
-Console.WriteLine(Solve((crabPos, targetPos) => Math.Abs(crabPos - targetPos)));
+Console.WriteLine(Solve(distance => distance));
 
 // Part 2
 // 1 + 2 + ... + (n-1) + n = n(n+1)/2
-Console.WriteLine(Solve((crabPos, targetPos) => {
-    long n = Math.Abs(crabPos - targetPos);
-    return n * (n + 1) / 2;
-}));
+Console.WriteLine(Solve(distance => distance * (distance + 1) / 2));
---
+++ day10/Program.cs
@@ -0,0 +1,50 @@
+﻿var input = File.ReadAllLines("input.txt");
+int part1 = 0;
+var part2 = new List<long>();
+
+foreach (var line in input)
+{
+    var opened = new Stack<char>();
+    bool corrupted = false;
+
+    foreach (char c in line)
+    {
+        if ("([{<".Contains(c))
+        {
+            opened.Push(c);
+        }
+        else
+        {
+            char o = opened.Pop();
+            if (o == '(' && c == ')' || o == '[' && c == ']' || o == '{' && c == '}' || o == '<' && c == '>') continue;
+            corrupted = true;
+            part1 += c switch {
+                ')' => 3,
+                ']' => 57,
+                '}' => 1197,
+                '>' => 25137,
+            };
+            break;
+        }
+    }
+
+    if (!corrupted)
+    {
+        long score = 0;
+        while (opened.Any())
+        {
+            score *= 5;
+            score += opened.Pop() switch {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4
+            };
+        }
+        part2.Add(score);
+    }
+}
+
+Console.WriteLine(part1);
+part2.Sort();
+Console.WriteLine(part2[part2.Count / 2]);
